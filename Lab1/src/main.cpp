42f4b81ea49407033c1d6d8d3d81dca2f8695537
kruyneg <kruyneg@gmail.com>
2024-10-01 17:00:49 +0300

    change random point

--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -8,10 +8,18 @@ sf::Vector2f randPoint(const sf::Window& win) {
     std::mt19937 random_generator(now.time_since_epoch().count());
     float pos = random_generator() % win.getSize().x;
     if (pos > win.getSize().x) { std::cout << "panic!\n"; exit(-1); }
-    return {
-        random_generator() % win.getSize().x,
-        random_generator() % win.getSize().y
-    };
+    return 
+        (random_generator() % 2) ? 
+        sf::Vector2f{
+            (random_generator() % 2) ? (win.getSize().x - 300) : 0,
+            // random_generator() % win.getSize().x,
+            random_generator() % (win.getSize().y - 100)
+        }:
+        sf::Vector2f{
+            // random_generator() % win.getSize().x,
+            random_generator() % (win.getSize().x - 300),
+            (random_generator() % 2) ? (win.getSize().y - 100) : 0
+        };
 }
 
 /// @brief          Интерполяция между точками
@@ -40,7 +48,9 @@ int main() {
 
 
     sf::CircleShape hexagon(50, 6);
+    hexagon.setOutlineThickness(5);
     hexagon.setOutlineColor(sf::Color::White);
+    hexagon.setFillColor(sf::Color::Red);
     // hexagon.setPosition(static_cast<sf::Vector2f>(window.getSize()) / 2.0f);
 
     sf::Vector2f startPosition{static_cast<sf::Vector2f>(window.getSize()) / 2.0f};
@@ -52,7 +62,7 @@ int main() {
     sf::Transform matrix; // матрица трансформаций
 
     sf::Clock clock;
-    float moveTime = 1; // время за которое фигура переместится между точками
+    float moveTime = 3; // время за которое фигура переместится между точками
     while (window.isOpen()) {
 
         std::cout << "\r                \r" <<  endPosition.x << ' ' << endPosition.y << std::flush;
@@ -69,9 +79,9 @@ int main() {
             matrix = sf::Transform::Identity;
             matrix.translate(curPosition);
             if (direction)
-                matrix.scale(sf::Vector2f{2 * t, 2 * t});
+                matrix.scale(sf::Vector2f{1 + 2 * t, 1});
             else
-                matrix.scale(sf::Vector2f{2 * (1 - t), 2 * (1 - t)});
+                matrix.scale(sf::Vector2f{1 + 2 * (1 - t), 1});
 
             window.clear();
             window.draw(hexagon, matrix);
